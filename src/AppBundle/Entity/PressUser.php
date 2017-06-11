984534d3bddadbbd11f5c911fae890b5f17f7ad3
Jorge Guerra <jorguerra@gmail.com>
2017-06-11 01:43:01 -0400

    FOSUserBundle

---
+++ src/AppBundle/Entity/PressUser.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * PressUser
+ *
+ * @author Jorge Guerra <jorguerra@gmail.com>
+ */
+
+use FOS\UserBundle\Model\User;
+
+class PressUser extends User
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     */
+    protected $lastName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return PressUser
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return PressUser
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+}
+
